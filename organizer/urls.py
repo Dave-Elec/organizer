bd2120f0d7d6433dab482c4d3fa22bb4346e6d7f
Dawit Abate <dawitabate2@gmail.com>
2021-11-07 09:29:43 +0300

    added auth urls

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import urls as auth_urls
+from django.urls import path, include
 from todo import views as todo_views
 
 urlpatterns = [
     path('', todo_views.index, name='tasks'),
     path('remove/<int:task_id>', todo_views.remove_task, name='remove'),
     path('admin/', admin.site.urls),
+    path('', include(auth_urls)),
 ]
